f9b5f2c4c67adcc881e03ffe9352096735fa86f2
garezana <virnaliz.cruz@weecology.org>
2019-11-06 20:57:18 -0500

    Rmarkdown addition and txt files update

--- scripts/renamefiles.py
+++ scripts/renamefiles.py
@@ -1,15 +1,19 @@
-
-import os, csv
+import os, csv, glob
 import pandas as pd
 import numpy as np
 
 files = [f for f in os.listdir('.') if os.path.isfile(f)]
 for f in files:
+    #Only for txt files
     if f.endswith(".txt"):  
-        df=pd.read_table(f, encoding='utf-16', sep="\t") 
-        year=np.array(df["PD"].values).tolist()  
-        print(year[0])
-        journame=df["BN"].values
-        name=journame[1].replace(" ", ".")
+        #Read txt file
+        df=pd.read_table(f, encoding='utf-16', sep="\t")
+        #Drop all empty columns
+        df.dropna(axis=1,how='all',inplace=True)
+        #df.drop(df.columns[0], axis=1)
+        #Write df as csv without null columns
+#df.to_csv(file_name, sep='\t')
+        df.to_csv(os.path.splitext(f)[0]+ "1.txt", sep='\t')
 
-        os.rename(f, name + "." +  str(year[1]) + ".txt")
+
+
